139eeaa56b1b92a85e4f8cc7e79c854e11e53d1a
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-06-19 07:41:22 -0700

    init demo

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# Flux Demo
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+#[lr::sig(fn(bool[true]) -> ())]
+pub fn assert(_b: bool) {}
+
+#[lr::sig(fn(x:i32) -> i32{v: v > x})]
+pub fn inc(x: i32) -> i32 {
+    assert(100 < 20);
+    x + 1
+}
+
+fn main() {
+    return;
+}
