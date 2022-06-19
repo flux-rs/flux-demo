41ea2ccd82cc0f524b9bcd65d255bf838b76421c
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-06-19 10:51:06 -0700

    init demo

---
+++ src/basics.rs
@@ -0,0 +1,53 @@
+// #![feature(register_tool)]
+// #![register_tool(lr)]
+
+// --------------------------------------------------------------------
+// Booleans refined with an index -------------------------------------
+// --------------------------------------------------------------------
+
+// output type = post-condition, specifies function returns 'true'
+#[lr::sig(fn () -> bool[true])]
+pub fn tt() -> bool {
+    return true;
+}
+
+// output type = post-condition, specifies function returns 'false'
+#[lr::sig(fn () -> bool[false])]
+pub fn ff() -> bool {
+    return false;
+}
+
+// An `assert` function, whose precondition expects only `true`
+
+#[lr::sig(fn(bool[true]) -> ())]
+pub fn assert(_b: bool) {}
+
+fn _test_assert() {
+    assert(1 < 2);
+    // assert(10 < 2);
+}
+
+// --------------------------------------------------------------------
+// Integers refined with an index -------------------------------------
+// --------------------------------------------------------------------
+
+// output type says the function returns 5
+#[lr::sig(fn() -> i32[5])]
+pub fn five() -> i32 {
+    5
+}
+
+#[lr::sig(fn (n: i32) -> i32[n + 1])]
+pub fn inc(n: i32) -> i32 {
+    n + 1
+}
+
+fn _test_inc() {
+    assert(inc(10) == 11);
+}
+
+// #[lr::sig(fn(x:i32) -> i32{v: v > x})]
+// pub fn inc(x: i32) -> i32 {
+//     assert(100 < 20);
+//     x + 1
+// }
